be5e431243e1f9fa6ebdef1a90b58a2d49fdcb02
Aidan Traboulay <aidantraboulay13@gmail.com>
2020-10-03 18:38:27 -0400

    added data sets

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+import pandas as pd
+import csv
+from sklearn.linear_model import LinearRegression
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    df = pd.read_csv('data/business_dynamics.csv')
 
+    
 
     #Render main page
-    return render_template("index.html")
+    return render_template("index.html", tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == "__main__":
     app.run(debug = True)
